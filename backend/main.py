c575442157cfa9bef988c21ff8c6fd4498003ed8
Danila <danilaursu965@gmail.com>
2026-06-16 13:15:57 +0300

    Setting the main.py file and checking the backend side of the app

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,41 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+import models , schemas
+from database import get_db , engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/all_films")
 def main_page():
-    return {"": ""}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
